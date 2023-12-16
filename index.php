5989f263a0f7aa100aea9c9f85da78c4e5e5e3f5
Tamara Dwi R <100558933+tamaradona@users.noreply.github.com>
2023-12-16 18:21:10 +0700

    Update and rename index.php to no4.php

--- index.php
+++
@@ -1,1 +0,0 @@
-<a href="session.php">Buat Session</a> | <a href="local_storage.php">Buat Local Storage</a>
---
+++ no4.php
@@ -0,0 +1,1 @@
+<a href="no4session.php">Buat Session</a> | <a href="local_storage.php">Buat Local Storage</a>
