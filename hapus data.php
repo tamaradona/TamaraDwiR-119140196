b19a0e7e3d938df0fecbc39ee3134efef58245e3
Tamara Dwi R <100558933+tamaradona@users.noreply.github.com>
2025-04-25 21:31:14 +0700

    Delete hapus data.php

--- hapus data.php
+++
@@ -1,7 +0,0 @@
-    <?php
-    include 'koneksi1.php';
-    $no = $_GET['no'];
-    $query = mysqli_query($mysqli,"delete from db_karyawan1 where no='$no' ");
-    
-    header('location:no3DataKaryawan.php');
-    ?>
